3241ee42427406b56a9ec42f4891f9ddd7d5c250
user1 <user1@DESKTOP-I3LB9L3>
2024-04-29 10:01:41 +0900

    자바0429

---
+++ java/05_array2/src/edu/kh/array2/practice/PracticeRun.java
@@ -0,0 +1,20 @@
+package edu.kh.array2.practice;
+
+public class PracticeRun {
+	public static void main(String[] args) {
+		Array2Practice arrEx1 = new Array2Practice();
+		//arrEx1.practice1();
+		//arrEx1.practice2();
+		//arrEx1.practice3();
+		//arrEx1.practice4();
+		//arrEx1.practice5();
+		//arrEx1.practice6();
+		//arrEx1.practice7();
+		//arrEx1.practice8();
+		//arrEx1.practice9();
+		//arrEx1.practice10();
+		arrEx1.bingoGame();
+		
+	}
+
+}
